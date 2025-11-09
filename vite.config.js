549d81ae4f2543c41d05371268a822345f492dbf
Abaquio <matt.sepulveda00@gmail.com>
2025-11-09 16:59:13 -0300

    act ghpages

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
+const isProd = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
+  },
+  // 👇 cambia automáticamente según entorno
+  base: isProd ? '/fideliza-plus/' : '/',
+  server: {
+    port: 5173,
+    open: true,
+    proxy: isProd
+      ? undefined // en producción no hay backend local
+      : {
+          '/api': {
+            target: 'http://localhost:3000',
+            changeOrigin: true,
+          },
+        },
   },
 })
