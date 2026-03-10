4ae3c33e8467269e3bb830dc279cef975ca69572
Abaquio <matt.sepulveda00@gmail.com>
2026-03-09 23:49:06 -0300

    update: ruta para los bordados

--- backend/src/routes/public.routes.js
+++ backend/src/routes/public.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { registrarClientePublico } from "../controllers/public.controller.js";
+import { registrarClientePublico, solicitarBordado } from "../controllers/public.controller.js";
 
 const router = Router();
 
-// Esta ruta es totalmente abierta
+// Esta ruta es totalmente abierta (Registro Fideliza+)
 router.post("/registro", registrarClientePublico);
+
+// ✅ NUEVO: Ruta abierta para recibir las solicitudes de bordado
+router.post("/bordado", solicitarBordado);
 
 export default router;
