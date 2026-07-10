7020baae7252c7a3e349c25195f6363a867508cb
Abaquio <matt.sepulveda00@gmail.com>
2026-07-10 14:04:28 -0400

    fix: cambios para railway

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,13 @@
 import { app } from "./app.js";
 import { env } from "./config/env.js";
 
-app.listen(env.PORT, () => {
+// ✅ Railway inyecta dinámicamente process.env.PORT. 
+// Nos aseguramos de tomarlo, y si no existe (como en tu PC local), usamos el de tu env.js
+const PORT = process.env.PORT || env.PORT || 4000;
+
+// ✅ Agregamos "0.0.0.0" para asegurar que escuche tráfico externo en contenedores de Railway
+app.listen(PORT, "0.0.0.0", () => {
   console.log(
-    `🚀 Backend Fideliza+ escuchando en puerto ${env.PORT} (${env.NODE_ENV})`
+    `🚀 Backend Fideliza+ escuchando en puerto ${PORT} (${env.NODE_ENV || 'desarrollo'})`
   );
 });
